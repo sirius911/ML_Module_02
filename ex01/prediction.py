f0dbab4ca6b1c289d2a11af2586942c705c80cc2
sirius911 <cyrille.lorin@yahoo.fr>
2022-10-19 19:41:19 +0200

    ADD:ex01

---
+++ ex01/prediction.py
@@ -0,0 +1,21 @@
+import numpy as np
+
+
+def predict_(x, theta):
+    """Computes the prediction vector y_hat from two non-empty numpy.array.
+    Args:
+        x: has to be an numpy.array, a matrix of dimension m * n.
+        theta: has to be an numpy.array, a vector of dimension (n + 1) * 1.
+    Return:
+        y_hat as a numpy.array, a vector of dimension m * 1.
+        None if x or theta are empty numpy.array.
+        None if x or theta dimensions are not matching.
+        None if x or theta is not of expected type.
+    Raises:
+        This function should not raise any Exception.
+    """
+    try:
+        x_1 = np.c_[np.ones(x.shape[0]), x]
+        return x_1.dot(theta)
+    except Exception:
+        return None
