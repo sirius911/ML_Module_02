9562c2e493b573476ef23a99bc3b263ae77ce7ae
clorin <clorin@student.42.fr>
2022-11-08 15:08:19 +0100

    ADD: min_max in mormalisation and analyzer in 3D

--- ex10/benchmark_train.py
+++ ex10/benchmark_train.py
@@ -5,6 +5,28 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import pickle
 
+def minmax(x):
+    """Computes the normalized version of a non-empty numpy.ndarray using the min-max standardization.
+    Args:
+        x: has to be an numpy.ndarray, a vector.
+    Returns:
+        x’ as a numpy.ndarray.
+        None if x is a non-empty numpy.ndarray or not a numpy.ndarray.
+    Raises:
+        This function shouldn’t raise any Exception.
+    """
+    if not isinstance(x, np.ndarray):
+        print("Error in minmax: not numpy.array")
+        return None
+    if len(x.shape) == 0 or len(x.shape) > 2:
+        print("Error in minmax: bad shape")
+        return None
+    if len(x.shape) == 2 and x.shape[1] != 1:
+        print("Error in minmax: bad shape")
+        return None
+    ret = np.array(x - np.min(x)) / (np.max(x) - np.min(x))
+    return (ret)
+
 def zscore(x):
     """Computes the normalized version of a non-empty numpy.ndarray using the z-score standardization.
     Args:
@@ -22,7 +44,7 @@ def zscore(x):
     if len(x.shape) == 2 and x.shape[1] != 1:
         return None
     try:
-        return ((x - np.mean(x)) / np.std(x))
+        return minmax((x - np.mean(x)) / np.std(x))
     except Exception:
         return None
 
@@ -52,17 +74,34 @@ if __name__ == "__main__":
     prod_distance = data.prod_distance.values.reshape(-1, 1)
     time_delivery = data.time_delivery.values.reshape(-1, 1)
    
-    figure, axis = plt.subplots(3, figsize=(20, 10))
+    # figure, axis = plt.subplots(3, figsize=(20, 10))
+
+    # axis[0].scatter(weight, target, c='blue')
+    # axis[0].set_xlabel("weight in ton")
+    # axis[1].scatter(prod_distance, target, c='red')
+    # axis[1].set_xlabel("dist in Mkm")
+    # axis[2].scatter(time_delivery, target, c='orange')
+    # axis[2].set_xlabel("time in days")
+    # plt.show()
     
+    # 3D
+    fig = plt.figure(figsize=(12, 20))
+    ax = fig.add_subplot(111, projection='3d')
+    ax.set_title('Analyzer')
+    ax.set_xlabel('weight')
+    ax.set_ylabel('distance')
+    ax.set_zlabel('time')
+    min_target = target.min()
+    max_target = target.max()
+    taille = target - min_target
+    taille = taille / (max_target - min_target) * 100
+    p = ax.scatter(weight, prod_distance, time_delivery, s=taille, c=target, cmap='viridis', vmin = min_target, vmax = max_target)
+    cbar = plt.colorbar(p)
+    cbar.set_label("price of the order (in trantorian unit)", labelpad=+1)
 
-    axis[0].scatter(weight, target, c='blue')
-    axis[0].set_xlabel("weight in ton")
-    axis[1].scatter(prod_distance, target, c='red')
-    axis[1].set_xlabel("dist in Mkm")
-    axis[2].scatter(time_delivery, target, c='orange')
-    axis[2].set_xlabel("time in days")
+
     plt.show()
-
+    
     # split dataset
     x_train, x_test, y_train, y_test = data_spliter(Xs, target.reshape(-1,1), 0.8)
     
@@ -84,7 +123,7 @@ if __name__ == "__main__":
         theta = np.random.rand(4, 1)
         mylr = MyLR(thetas=theta, alpha=1e-2, max_iter=1000000, progress_bar=True)
         mylr.fit_(add_polynomial_features(weight_train_norm, 3), y_train)
-        # mylr.thetas = np.array([[ 1.20893361e-01], [ 9.47634630e-01], [-1.18295546e-01], [-6.23921560e-04], [-1.46523240e-03]])
+        # mylr.thetas = np.array([[ 0.09278846], [ 0.99722145], [-0.25256595], [-0.02529634]])
         print(mylr.thetas)
         print("save mylr ...", end='')
         pickle.dump( mylr, open( "weight.pickle", "wb" ) )
@@ -111,8 +150,8 @@ if __name__ == "__main__":
     except FileNotFoundError:
         theta = np.random.rand(5, 1)
         mylr = MyLR(thetas=theta, alpha=1e-2, max_iter=1000000, progress_bar=True)
-        # mylr.fit_(add_polynomial_features(distance_train_norm, 4), y_train)
-        mylr.thetas = np.array([[-0.28295574], [-0.36618452], [ 0.32636224], [ 0.15421623], [-0.02273116]])
+        mylr.fit_(add_polynomial_features(distance_train_norm, 4), y_train)
+        # mylr.thetas = np.array([[ 0.59534824], [-0.15758319], [-0.61912105], [ 0.1995424 ], [ 0.68405988]])
         print(mylr.thetas)
         print("save mylr ...", end='')
         pickle.dump( mylr, open( "distance.pickle", "wb" ) )
@@ -141,7 +180,7 @@ if __name__ == "__main__":
         theta = np.random.rand(5, 1)
         mylr = MyLR(thetas=theta, alpha=1e-2, max_iter=1000000, progress_bar=True)
         mylr.fit_(add_polynomial_features(time_train_norm, 4), y_train)
-        # mylr.thetas = np.array([[ 0.00348599], [ 0.02234647], [ 0.00741201], [-0.01350792], [-0.00617828]])
+        # mylr.thetas = np.array([[ 0.49552787], [-0.05909071], [ 0.20255927], [-0.18875593], [ 0.03461856]])
         print(mylr.thetas)
         print("save mylr ...", end='')
         pickle.dump( mylr, open( "time.pickle", "wb" ) )
